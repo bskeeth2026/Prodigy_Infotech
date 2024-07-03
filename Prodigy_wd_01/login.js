67c9b822b66d5c065d99706e6ce475a9ff3b491d
bskeeth2026 <155647814+bskeeth2026@users.noreply.github.com>
2024-07-03 20:35:57 +0530

    Add files via upload

---
+++ Prodigy_wd_01/login.js
@@ -0,0 +1,5 @@
+function login() {
+    const usernameInput = document.getElementById("username").value;
+    const passwordInput = document.getElementById("password").value;
+    window.location.href = "mini.html";
+}
